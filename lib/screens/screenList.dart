b1c488049680a2b81c8805a823b868fc7d1f3ef8
Enzo CONTY <contact@enzoconty.dev>
2020-06-08 22:26:30 +0200

    Header and footer + tabs systems

---
+++ lib/screens/screenList.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+List<Container> screenList = [
+  Container(
+    width: 500,
+    height: 900,
+    color: Colors.yellow,
+  ),
+  Container(
+    width: 500,
+    height: 900,
+    color: Colors.green,
+  ),
+  Container(
+    width: 500,
+    height: 900,
+    color: Colors.blue,
+  ),
+  Container(
+    width: 500,
+    height: 900,
+    color: Colors.red,
+  ),
+  Container(
+    width: 500,
+    height: 900,
+    color: Colors.pink,
+  ),
+];
---
+++ lib/widgets/buttonTabs.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ButtonTabs extends StatefulWidget {
+  String title;
+  bool isSelected;
+
+  ButtonTabs(this.title, this.isSelected);
+
+  @override
+  _ButtonTabsState createState() => _ButtonTabsState();
+}
+
+class _ButtonTabsState extends State<ButtonTabs> {
+  Color selectedColor;
+
+  @override
+  void initState() {
+    selectedColor = Colors.white;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+      decoration: BoxDecoration(
+          color: widget.isSelected ? selectedColor : Colors.transparent,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(5), topRight: Radius.circular(5))),
+      child: Center(child: Text(widget.title)),
+    );
+  }
+}
