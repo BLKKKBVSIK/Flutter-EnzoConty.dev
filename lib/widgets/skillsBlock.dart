3c3dbe6901ca89f983790ceafe0411661ccf8398
Enzo CONTY <contact@enzoconty.dev>
2020-06-13 16:36:18 +0200

    Fixed skills progressBar

--- lib/widgets/skillsBlock.dart
+++ lib/widgets/skillsBlock.dart
@@ -1,72 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_enzoconty/misc/kColors.dart';
 
-class SkillsBlock extends StatelessWidget {
+class SkillsBlock extends StatefulWidget {
+
+  @override
+  _SkillsBlockState createState() => _SkillsBlockState();
+}
+
+class _SkillsBlockState extends State<SkillsBlock> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: kDarkBlue,
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-          ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Skills",
-                  style: TextStyle(color: Colors.white, fontSize: 32.0),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20.0),
-                  child: Divider(
-                    color: Colors.white,
-                    thickness: 2.0,
-                  ),
-                ),
-                Center(
-                    child: Column(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: kDarkBlue,
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    skillStickChart("Front-end", 0.70, Colors.green),
-                    skillStickChart("Back-end", 0.60, kGreen),
-                    skillStickChart("Databases", 0.55, kGreen),
-                    skillStickChart("Mobile Apps", 0.70, Colors.green),
-                    skillStickChart("Sys Admin", 0.40, Colors.yellow),
-                    skillStickChart("Reverse engineering", 0.35, Colors.red),
-                    skillStickChart("API", 0.65, kGreen),
-                    skillStickChart("Team Work", 0.60, kGreen),
                     Text(
-                      "...and much more.",
-                      style: TextStyle(
+                      "Skills",
+                      style: TextStyle(color: Colors.white, fontSize: 32.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: Divider(
                         color: Colors.white,
-                        fontSize: 16.0,
+                        thickness: 2.0,
                       ),
                     ),
-                    InkWell(
-                      onTap: () {
-
-                      },
-                      child: Text(
-                        "Click here to get the complete list",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                          decoration: TextDecoration.underline,
+                    Center(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        skillStickChart("Front-end", 0.70, Colors.green),
+                        skillStickChart("Back-end", 0.60, kGreen),
+                        skillStickChart("Databases", 0.55, kGreen),
+                        skillStickChart("Mobile Apps", 0.70, Colors.green),
+                        skillStickChart("Sys Admin", 0.40, Colors.yellow),
+                        skillStickChart(
+                            "Reverse engineering", 0.35, Colors.red),
+                        skillStickChart("API", 0.65, kGreen),
+                        skillStickChart("Team Work", 0.60, kGreen),
+                        Text(
+                          "...and much more.",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
                         ),
-                      ),
-                    ),
+                        InkWell(
+                          onTap: () {
+                            print(constraints.maxHeight);
+                          },
+                          child: Text(
+                            "Click here to get the complete list",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ))
                   ],
-                ))
-              ],
+                ),
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
@@ -76,15 +87,18 @@ Widget skillStickChart(String skillName, double skillAmount, Color chartColor) {
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          skillName,
-          style: TextStyle(color: Colors.white, fontSize: 16.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            skillName,
+            style: TextStyle(color: Colors.white, fontSize: 16.0),
+          ),
         ),
         Padding(
           padding: const EdgeInsets.only(bottom: 21),
           child: Container(
             height: 16,
-            width: 320,
+            padding: EdgeInsets.symmetric(horizontal: 10),
             child: Stack(
               children: <Widget>[
                 Container(
@@ -98,7 +112,9 @@ Widget skillStickChart(String skillName, double skillAmount, Color chartColor) {
                   child: Container(
                     decoration: BoxDecoration(
                       color: chartColor,
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10)),
                     ),
                   ),
                 )
