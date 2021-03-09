f4ec2d86ef1e77e22fb860b88fa3e65311918686
Enzo CONTY <contact@enzoconty.dev>
2021-03-09 10:32:17 +0100

    :lipstick: UI Theming and refactor.

--- lib/misc/k_colors.dart
+++ lib/misc/k_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color kBlue = Color.fromRGBO(32, 156, 238, 1);
+const Color kBlue = Color.fromRGBO(50, 152, 220, 1);
 const Color kGreen = Color.fromRGBO(0, 209, 178, 1);
 const Color kDarkBlue = Color.fromRGBO(50, 115, 220, 1);
 const Color kDarkerBlue = Color.fromRGBO(18, 83, 126, 1);
